245881130ffd10874c018198b34e860b99092bbb
Nataliia Kovaliv <nataliiakovaliv95@gmail.com>
2022-04-08 23:48:43 +0300

    fix deploy

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use(express.json());
 
 app.use('/api/announcements', announcementsRoutes);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
